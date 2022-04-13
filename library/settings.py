8130b330c335e36779300067d4b49bfd79b48e37
Renata Hurtado <renatah2198@gmail.com>
2022-04-13 17:35:48 -0500

    Add postgres service

--- library/settings.py
+++ library/settings.py
@@ -109,6 +109,29 @@ DATABASES = {
     }
 }
 
+# if os.environ.get('GITHUB_WORKFLOW'):
+#     DATABASES = {
+#         'default': {
+#            'ENGINE': 'django.db.backends.postgresql',
+#            'NAME': 'github_actions',
+#            'USER': 'postgres',
+#            'PASSWORD': 'postgres',
+#            'HOST': 'postgres',
+#            'PORT': '5432',
+#         }
+#     }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'github_actions',
+#         'USER': 'radorado',
+#         'PASSWORD': 'radorado',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+
 if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
@@ -116,7 +139,7 @@ if os.environ.get('GITHUB_WORKFLOW'):
            'NAME': 'github_actions',
            'USER': 'postgres',
            'PASSWORD': 'postgres',
-           'HOST': 'postgres',
+           'HOST': '127.0.0.1',
            'PORT': '5432',
         }
     }
