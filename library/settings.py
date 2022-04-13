ea4c3b76a6667b0128c0af988514bf82ac8a941c
Renata Hurtado <renatah2198@gmail.com>
2022-04-13 16:35:42 -0500

    Try new env

--- library/settings.py
+++ library/settings.py
@@ -161,7 +161,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=int(env.get('JWT_TOKEN_TIME'))),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ALGORITHM': 'HS256',
