dd3b93641c6038bf7049b84b613d4ea82ec9acf2
Renata Hurtado <renatah2198@gmail.com>
2022-04-13 17:05:52 -0500

    Change databases

--- library/settings.py
+++ library/settings.py
@@ -103,10 +103,22 @@ DATABASES = {
         'NAME': 'renata_hurtadodb',
         'USER': 'renata_hurtado',
         'PASSWORD': 'Renata.2198',
-        'HOST': 'postgres_db33',
+        'HOST': '127.0.0.1',
         'PORT': '5533',
     }
 }
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
